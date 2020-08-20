fd4f120fbbb55e104fb842acd8d09bc6ce69447b
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-08-19 23:59:26 -0300

    CHORE: Mostra rastro metodo 1

--- Main.js
+++ Main.js
@@ -4,43 +4,10 @@ function log(text) {
 	console.error(text);
 }
 
-function way(direction) {
-	log(direction);
-
-	if (direction > 4) {
-		direction = 1;
-	}
-
-	if (direction === 1) {
-		log("Subindo");
-	}
-
-	if (direction === 2) {
-		log("Avançando");
-	}
-
-	if (direction === 3) {
-		log("Descendo");
-	}
-
-	if (direction === 4) {
-		log("Recuando");
-	}
-}
-
 function main() {
 	let steps = 0;
 	let x = 0;
 	let y = 0;
-
-	// Valor que indica para que lado o robô esta indo, desta forma podemos 
-	// calcular sua posição atual.
-	let direction = 1;
-	// direction (1) => Subindo
-	// direction (2) => Avançando
-	// direction (3) => Descendo
-	// direction (4) => Recuando
-	
 
 	// Seleção de método:
 	let method = 1;
@@ -48,46 +15,108 @@ function main() {
 	// Method ( 2 ) => Método da mão ESQUERDA ('')
 	// Method ( 3 ) => Método de Tremaux
 
+	let direction = 1;
+	// Direction ( 1 ) => SUBINDO
+	// Direction ( 2 ) => DIREITA
+	// Direction ( 3 ) => DESCENDO
+	// Direction ( 4 ) => ESQUERDA
+
+	function Direction() {
+		if (direction > 4) {
+			direction = 1;
+			// y--;
+		}
+		if (direction < 1) {
+			direction = 4;
+		}
+
+		if (direction === 1) {
+			log("SUBINDO");
+			y++;
+			log("Posição: (" + x + "," + y + ")");
+			log("");
+			IA.setColor(x, y, 'G');
+			direction = 1;
+		}
+		if (direction === 2) {
+			log("DIREITA");
+			x++;
+			log("Posição: (" + x + "," + y + ")");
+			log("");
+			IA.setColor(x, y, 'G');
+		}
+		if (direction === 3) {
+			log("DESCENDO");
+			y--;
+			log("Posição: (" + x + "," + y + ")");
+			log("");
+			IA.setColor(x, y, 'G');
+		}
+		if (direction === 4) {
+			log("ESQUERDA");
+			x--;
+			log("Posição: (" + x + "," + y + ")");
+			log("");
+			IA.setColor(x, y, 'G');
+		}
+	}
+
 	if (method === 1) {
 		log("Iniciando método da mão DIREITA...");
 		log("");
 		log("");
-		IA.setColor(x, y, 'g');
-		IA.setText(x, y, "Start");
+		IA.setColor(0, 0, 'g');
+		IA.setText(0, 0, "Start");
 
 		while (true) {
 			while (!IA.wallRight()) {
+				log("");
 				log("Buscando parede na direita.");
 
 				IA.turnRight();
 				IA.moveForward();
 
+				direction++;
+				Direction(direction);
+
 				if (!IA.wallFront() && IA.wallRight()) {
+					log("");
 					log("Frente limpa, seguindo.");
+
 					IA.moveForward();
+					Direction(direction);
 				}
 			}
 
 			while (IA.wallRight() && IA.wallFront() && IA.wallLeft()) {
+				log("");
 				log("Beco sem saida, voltando.");
+
 				IA.turnLeft();
 				IA.turnLeft();
 				IA.moveForward();
+
+				direction = direction - 2;
+				Direction(direction);
 			}
 
 			while (IA.wallRight() && IA.wallFront() && !IA.wallLeft()) {
-				log("Esquerda livre, girando e seguindo.");
 				log("");
+				log("Esquerda livre, girando e seguindo.");
+
 				IA.turnLeft();
 				IA.moveForward();
+
+				direction--;
+				Direction(direction);
 			}
 
 			if (IA.wallRight() && !IA.wallFront()) {
+				log("");
 				log("Mão na parede,");
 
 				IA.moveForward();
-
-				way(direction);
+				Direction(direction);
 			}
 
 			steps++;
@@ -95,44 +124,60 @@ function main() {
 	}
 
 	if (method === 2) {
-		log("Iniciando método da mão ESQUERDA...");
 		log("");
+		log("Iniciando método da mão ESQUERDA...");
 		IA.setColor(0, 0, 'g');
 		IA.setText(0, 0, "Start");
 
 		while (true) {
 			while (!IA.wallLeft()) {
-				log("Buscando parede na esquerda");
 				log("");
+				log("Buscando parede na esquerda");
 				IA.turnLeft();
 				IA.moveForward();
 
+				direction--;
+				Direction(direction);
+
 				if (!IA.wallFront() && IA.wallLeft()) {
-					log("Frente limpa, seguindo");
 					log("");
+					log("Frente limpa, seguindo");
+
 					IA.moveForward();
+					Direction(direction);
 				}
 			}
 
 			while (IA.wallLeft() && IA.wallFront() && IA.wallRight()) {
-				log("Beco sem saida, Voltando");
 				log("");
+				log("Beco sem saida, Voltando");
+
 				IA.turnRight();
 				IA.turnRight();
 				IA.moveForward();
+
+				direction = direction + 2;
+				Direction(direction);
 			}
 
 			while (IA.wallLeft() && IA.wallFront() && !IA.wallRight()) {
-				log("Direita livre, girando e seguindo");
 				log("");
+				log("Direita livre, girando e seguindo");
+
 				IA.turnRight();
 				IA.moveForward();
+
+				direction++;
+				Direction(direction);
 			}
 
 			if (IA.wallLeft() && !IA.wallFront()) {
-				log("Mão na parede, Seguindo");
 				log("");
+				log("Mão na parede, Seguindo");
+
 				IA.moveForward();
+
+				Direction(direction);
 			}
 
 			steps++;
@@ -152,7 +197,3 @@ function main() {
 }
 
 main();
-
-
-// log("Posição: (" + x + "," + y + ")");
-// log("");
