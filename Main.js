36dd8a744a2702baa6a4fab55d5b7fed9358b29d
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-08-21 20:50:44 -0300

    CHORE: Para no start

--- Main.js
+++ Main.js
@@ -8,9 +8,10 @@ function main() {
 	let steps = 0;
 	let x = 0;
 	let y = 0;
+	var round = 0;
 
 	// Seleção de método:
-	let method = 1;
+	const method = 2;
 	// Method ( 1 ) => Método da mão DIREITA ('')
 	// Method ( 2 ) => Método da mão ESQUERDA ('')
 	// Method ( 3 ) => Método de Tremaux
@@ -19,7 +20,7 @@ function main() {
 	// Direction ( 1 ) => SUBINDO
 	// Direction ( 2 ) => DIREITA
 	// Direction ( 3 ) => DESCENDO
-	// Direction ( 4 ) => ESQUERDA
+	// Direction ( 4 ) => ESQUERDA   
 
 	function Direction() {
 		if (direction === 5) {
@@ -41,7 +42,7 @@ function main() {
 			log("Posição: (" + x + "," + y + ")");
 			log("");
 			IA.setColor(x, y, 'G');
-			direction = 1;
+			// direction = 1;
 		}
 		if (direction === 2) {
 			log("DIREITA");
@@ -70,61 +71,69 @@ function main() {
 		log("Iniciando método da mão DIREITA...");
 		log("");
 		log("");
-		IA.setColor(0, 0, 'g');
+		IA.setColor(0, 0, 'D');
 		IA.setText(0, 0, "Start");
 
 		while (true) {
-			while (!IA.wallRight()) {
-				log("");
-				log("Buscando parede na direita.");
+			if (round !== 1) {
+				if (!IA.wallRight()) {
+					log("");
+					log("Buscando parede na direita.");
+	
+					IA.turnRight();
+					IA.moveForward();
+	
+					direction++;
+					Direction(direction);
+				}
 
-				IA.turnRight();
-				IA.moveForward();
-
-				direction++;
-				Direction(direction);
-
-				if (!IA.wallFront() && IA.wallRight()) {
+				else if (!IA.wallFront() && IA.wallRight()) {
 					log("");
 					log("Frente limpa, seguindo.");
 
 					IA.moveForward();
 					Direction(direction);
 				}
+	
+				else if (IA.wallRight() && IA.wallFront() && IA.wallLeft()) {
+					log("");
+					log("Beco sem saida, voltando.");
+	
+					IA.turnLeft();
+					IA.turnLeft();
+					IA.moveForward(); 
+	
+					direction = direction + 2;
+					Direction(direction);
+				}
+	
+				else if (IA.wallRight() && IA.wallFront() && !IA.wallLeft()) {
+					log("");
+					log("Esquerda livre, girando e seguindo.");
+	
+					IA.turnLeft();
+					IA.moveForward();
+	
+					direction--;
+					Direction(direction);
+				}
+	
+				else if (IA.wallRight() && !IA.wallFront()) {
+					log("");
+					log("Mão na parede,");
+	
+					IA.moveForward();
+					Direction(direction);
+				}
+	
+				steps++;
+
+				if (x === 0 && y === 0) {
+					round++;
+				}
+			} else {
+				return log("Volta encerrada, quantidade de passos: " + steps);
 			}
-
-			while (IA.wallRight() && IA.wallFront() && IA.wallLeft()) {
-				log("");
-				log("Beco sem saida, voltando.");
-
-				IA.turnLeft();
-				IA.turnLeft();
-				IA.moveForward();
-
-				direction = direction + 2;
-				Direction(direction);
-			}
-
-			while (IA.wallRight() && IA.wallFront() && !IA.wallLeft()) {
-				log("");
-				log("Esquerda livre, girando e seguindo.");
-
-				IA.turnLeft();
-				IA.moveForward();
-
-				direction--;
-				Direction(direction);
-			}
-
-			if (IA.wallRight() && !IA.wallFront()) {
-				log("");
-				log("Mão na parede,");
-
-				IA.moveForward();
-				Direction(direction);
-			}
-
-			steps++;
 		}
 	}
 
@@ -132,69 +141,80 @@ function main() {
 		log("Iniciando método da mão ESQUERDA...");
 		log("");
 		log("");
-		IA.setColor(0, 0, 'g');
+		IA.setColor(0, 0, 'D');
 		IA.setText(0, 0, "Start");
 
 		while (true) {
-			while (!IA.wallLeft()) {
-				log("");
-				log("Buscando parede na esquerda.");
+			if (round !== 1) {
+				if (!IA.wallLeft()) {
+					log("");
+					log("Buscando parede na esquerda.");
+	
+					IA.turnLeft();
+					IA.moveForward();
+	
+					direction--;
+					Direction(direction);
+				}
 
-				IA.turnLeft();
-				IA.moveForward();
-
-				direction--;
-				Direction(direction);
-
-				if (!IA.wallFront() && IA.wallLeft()) {
+				else if (!IA.wallFront() && IA.wallLeft()) {
 					log("");
 					log("Frente limpa, seguindo.");
 
 					IA.moveForward();
 					Direction(direction);
 				}
+	
+				else if (IA.wallLeft() && IA.wallFront() && IA.wallRight()) {
+					log("");
+					log("Beco sem saida, voltando.");
+	
+					IA.turnRight();
+					IA.turnRight();
+					IA.moveForward(); 
+	
+					direction = direction - 2;
+					Direction(direction);
+				}
+	
+				else if (IA.wallLeft() && IA.wallFront() && !IA.wallRight()) {
+					log("");
+					log("Direita livre, girando e seguindo.");
+	
+					IA.turnRight();
+					IA.moveForward();
+	
+					direction++;
+					Direction(direction);
+				}
+	
+				else if (IA.wallLeft() && !IA.wallFront()) {
+					log("");
+					log("Mão na parede,");
+	
+					IA.moveForward();
+					Direction(direction);
+				}
+	
+				steps++;
+				if (x === 0 && y === 0) {
+					round++;
+				}
+			} else {
+				return log("Volta encerrada, quantidade de passos: " + steps);
 			}
-
-			while (IA.wallLeft() && IA.wallFront() && IA.wallRight()) {
-				log("");
-				log("Beco sem saida, voltando.");
-
-				IA.turnRight();
-				IA.turnRight();
-				IA.moveForward();
-
-				direction = direction - 2;
-				Direction(direction);
-			}
-
-			while (IA.wallLeft() && IA.wallFront() && !IA.wallRight()) {
-				log("");
-				log("Direita livre, girando e seguindo.");
-
-				IA.turnRight();
-				IA.moveForward();
-
-				direction++;
-				Direction(direction);
-			}
-
-			if (IA.wallLeft() && !IA.wallFront()) {
-				log("");
-				log("Mão na parede,");
-
-				IA.moveForward();
-				Direction(direction);
-			}
-
-			steps++;
 		}
 	}
 
 	if (method === 3) {
 		log("Iniciando Método de Tremaux...");
 		log("");
-		API.setColor(0, 0, 'g');
-		API.setText(0, 0, "Start");
+		IA.setColor(0, 0, 'D');
+		IA.setText(0, 0, "Start");
+
+		while (true) {
+
+		}
 	}
 
 	else {
